5af1bf81af53805ca2ba2ac63ebfa1b6ba954604
RavjiRi <125327696+RavjiRi@users.noreply.github.com>
2025-03-09 22:45:04 +0000

    PEP changes, graphing optimization, and int fixes
    Added better commenting and code layout to fit with PEP.
    Changed the animate graphing code in graphprocess.py so that it doesn't replot all data points every animate.
    Large integers won't transmit on XML RPC due to overflow error, so dump the list in JSON and send to server subprocess...
    at the subprocess turn back into a list with JSON loads.

--- graphprocess.py
+++ graphprocess.py
@@ -8,11 +8,13 @@ There are two threads in this process:
 
 This is not intended to run by itself.
 
-Version: 4/3/25
+Version: 10/3/25
 
 Author: RavjiRi
 """
 from xmlrpc.server import SimpleXMLRPCServer
+from argparse import ArgumentParser
+from json import loads
 from threading import Thread
 from signal import SIGUSR1
 from subprocess import run
@@ -27,9 +29,23 @@ import matplotlib.animation as animation
 # configuration variables
 _plot_rate = 1 # how often matplotlib animation function is called to plot points (ms)
 _DEBUG = False
+PLOT_COLOUR = (0, 0, 1) # the plot colour
+parser = ArgumentParser()
+parser.add_argument('PORT') # first argument is port arg
+args = parser.parse_args()
+PORT = int(args.PORT)
 
 def process_running(pid: int):
-    # check if a process is running
+    '''Check if a process is running.
+
+    This calls the command line command ps to get all running processes
+    The result is then filtered with the grep command to see if the process is running or not
+
+    Args:
+        pid (int): the process id to check
+    Returns:
+        None
+    '''
     strpid = str(pid)
     p = run(['ps', '-e'], capture_output=True) # get all running processes
     # count how many processes are with the pid
@@ -39,12 +55,20 @@ def process_running(pid: int):
 
 _data_points = [] # store plot points that are sent
 def _on_data_recieved(data_sent: list):
+    '''Extend sent data to data points list.
+
+    Args:
+        data_sent (list): the data sent from the main process
+    Returns:
+        None
+    '''
     # extend sent data to data points list
-    _data_points.extend(data_sent)
+    # convert from JSON format
+    _data_points.extend(loads(data_sent))
     return True
 
 # create XML RPC server at localhost:8000 (broadcast address)
-_server = SimpleXMLRPCServer(("127.0.0.1", 8000), logRequests=False)
+_server = SimpleXMLRPCServer(("127.0.0.1", PORT), logRequests=False)
 _server.register_function(_on_data_recieved, "send_data")
 if _DEBUG:
     print("Graph server is running")
@@ -59,38 +83,53 @@ _ppid = os.getppid()
 # send user defined signal to the parent process to say server is ready
 os.kill(_ppid, SIGUSR1)
 
-# This function is called periodically from FuncAnimation
-def animate(i, xs, ys, plt, ax):
+def animate(i, x_points, y_points, plt, ax):
+    '''Plot and animate the on the graph.
+
+    This is called every _plot_rate ms
+
+    Args:
+        i (int): ???
+        x_points (list): the list of x points saved from the previous function call
+        y_points (list): the list of y points saved from the previous function call
+        plt (module): the matplotlib plot object
+        axes (axes): the matplotlib axes
+    Returns:
+        None
+    '''
     if not process_running(_ppid):
         # check if the parent process is still running
         # this is because IDLE does not close this process and its plot window
         # on the mac terminal the process and window closes but this is just an extra check
         sys.exit()
 
+    # get the end point and add it to the start of the lists
+    # this is so the points connect on the plot rather than an empty space
+    if x_points:
+        end_x = x_points[-1]
+        x_points.clear()
+        x_points.append(end_x)
+    if y_points:
+        end_y = y_points[-1]
+        y_points.clear()
+        y_points.append(end_y)
+
     for point in _data_points:
         # add x and y to lists
-        xs.append(point[0])
-        ys.append(point[1])
+        x_points.append(point[0])
+        y_points.append(point[1])
     # clear the list so the data isn't replotted next time
     _data_points.clear()
 
-    # Limit x and y lists to 20 items
-    #xs = xs[-20:]
-    #ys = ys[-20:]
+    # draw the lists
+    plt.plot(x_points, y_points, color=PLOT_COLOUR)
 
-    # Draw x and y lists
-    ax.clear()
-    ax.plot(xs, ys)
+    # apply plot formatting here
 
-    # Format plot
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-
-# Create figure for plotting
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-xs = []
-ys = []
-# Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys, plt, ax), interval=_plot_rate)
+# plot figure and axes
+fig, ax = plt.subplots()
+x_points = []
+y_points = []
+# create a func animation to call animate every _plot_rate ms
+ani = animation.FuncAnimation(fig, animate, fargs=(x_points, y_points, plt, ax), interval=_plot_rate)
 plt.show()
--- maingrapher.py
+++ maingrapher.py
@@ -7,9 +7,9 @@ Using threads does not work as matplotlib is not thread safe...
 and multiprocessing requires the main script importing the library to have a main guard which is unhelpful.
 So this library creates the plot using a subprocess and uses signals and xmlrpc to communicate data.
 
-HOW TO USE THIS
+Run this by import, this was made to work for the apple mac.
 
-Version: 4/3/25
+Version: 10/3/25
 
 Author: RavjiRi
 """
@@ -17,6 +17,7 @@ from threading import Event as _Event, Thread as _Thread, main_thread as _main_t
 from signal import signal as _signal, SIGUSR1 as _SIGUSR1
 from subprocess import Popen as _Popen, PIPE as _PIPE
 from xmlrpc.client import ServerProxy as _ServerProxy
+from json import dumps as _dumps
 from pathlib import Path as _Path
 from time import sleep as _sleep
 import sys as _sys
@@ -25,38 +26,31 @@ import sys as _sys
 _sync_frequency = 0.1 # how often to send the plot points to subprocess
 _DEBUG = False
 _isIDLE = True
-
-# create client proxy object
-_proxy = _ServerProxy("http://127.0.0.1:8000/")
-
-_stdout = _sys.stdout
-# using sys.stdout does not work on IDLE as it is replaced with another object
-if _isIDLE:
-    _stdout = _PIPE
-
-subprocess_ready = _Event()
-
-# set the threading event when the user defined signal is sent from the child process
-# n, frame = signal number, current stack frame though these are unused
-_signal(_SIGUSR1, lambda n, frame: subprocess_ready.set())
-
-process_dir = _Path(__file__).parent/'graphprocess.py'
-# using python3 doesn't work because it will open an older version than more recent version with pip
-p = _Popen(['/Library/Frameworks/Python.framework/Versions/3.13/bin/python3', process_dir], stdin=_PIPE, stdout=_sys.__stdout__, stderr=_sys.__stdout__)
-# wait for subprocess to startup the server
-# this will wait until the signal is sent from which the event is set
-subprocess_ready.wait()
-# cleanup event object
-del subprocess_ready
+_ADDRESS = 'http://127.0.0.1:' # local host broadcast address
+class configuration():
+    pass
+configs = configuration()
+configs.PORT = 8000 # python requests port, can use any UNPRIVLEDGED port
 
 _data_points = []  # store plot points that are about to be sent
-# send plot points with this function
-def _sync_send():
-    # send data points to subprocess
+def _sync_send(proxy: _ServerProxy):
+    '''Send data points to subprocess.
+
+    The secondary thread calls this function and starts a loop
+    This sends plot points from the XMLRPC client to the server on the subprocess
+
+    Args:
+        proxy (xmlrpc.client.ServerProxy): the XMLRPC client proxy to send data over
+    Returns:
+        None
+    '''
     while _main_thread().is_alive():
         if _DEBUG:
             print('sending')
-        _proxy.send_data(_data_points)
+        # send the data in JSON format
+        # XML RPC doesn't support large numbers due to overflow error
+        # sending a large string works however
+        proxy.send_data(_dumps(_data_points))
         # clear the list, do not set to an empty list
         # otherwise the list pointer is overwritten and a global variable is needed
         _data_points.clear()
@@ -64,14 +58,57 @@ def _sync_send():
 
 # exposed function to add data point to the plot
 def plot(x: int | float, y: int | float):
+    '''Add data point to the plot.
+
+    This adds a data point to the data points variable
+    The points will wait to be sent to the server subprocess
+
+    Args:
+        x (int | float): the x coordinate
+        y (int | float): the y coordinate
+    Returns:
+        None
+    '''
     _data_points.append((x, y))
 
-# create a new thread and start
-_sync_thread = _Thread(target=_sync_send)
-_sync_thread.start()
+def start():
+    '''Start the module.
+
+    This starts server subprocess which handles the matplotlib graph and the threads
+
+    Args:
+        None
+    Returns:
+        None
+    '''
+    # create client proxy object
+    proxy = _ServerProxy(_ADDRESS + str(configs.PORT))
+
+    subprocess_ready = _Event()
+
+    # set the threading event when the user defined signal is sent from the child process
+    # n, frame = signal number, current stack frame though these are unused
+    _signal(_SIGUSR1, lambda n, frame: subprocess_ready.set())
+
+    # this means graphprocess.py will have to be in the same directory as maingrapher.py
+    process_dir = _Path(__file__).parent/'graphprocess.py'
+    # using python3 doesn't work because it will open an older version than more recent version with pip
+    p = _Popen(['/Library/Frameworks/Python.framework/Versions/3.13/bin/python3',
+                process_dir, str(configs.PORT)],
+                stdin=_PIPE, stdout=_sys.__stdout__, stderr=_sys.__stdout__)
+    # wait for subprocess to startup the server
+    # this will wait until the signal is sent from which the event is set
+    subprocess_ready.wait()
+    # cleanup event object
+    del subprocess_ready
+
+    # create a new thread and start
+    sync_thread = _Thread(target=_sync_send, args=(proxy,))
+    sync_thread.start()
 
 # demo if not imported
 if __name__ == "__main__":
+    start()
     count = 0
     while True:
         count+=1
